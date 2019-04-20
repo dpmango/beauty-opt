aee2dc42bf8757edbcf9fbe43d2dc5fc7964e6e8
mars092 <mars0092@mail.ru>
2019-04-21 02:15:26 +0700

    homepage

--- src/components/homepage/_homepage.js
+++ src/components/homepage/_homepage.js
@@ -22,21 +22,80 @@
         return false;
       });
 
+      $('.block-main-tags__tag').on('click', function () {
+        $(this).toggleClass('block-main-tags__tag_active');
+        return false;
+      });
+
+      $('#show-all-categories').on('click', function () {
+        $('.categories-grid__category').removeClass('categories-grid__category_hidden');
+        $(this).parent().remove();
+        return false;
+      });
+
+      $('.wrap-mobile-search__ico').on('click', function () {
+        $('.mobile-search').addClass('mobile-search_active');
+      });
+
     },
   };
   APP.Plugins.Sliders = {
     init: function () {
+
       new Swiper('[main-slider]', {
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.swiper-button-next.swiper-button-next__main-slider',
+          prevEl: '.swiper-button-prev.swiper-button-prev__main-slider',
         },
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination.swiper-pagination__main-slider',
           clickable: true,
         },
         loop: true,
       });
+
+      new Swiper('[auto-width-slider]', {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        navigation: {
+          nextEl: '.swiper-button-next.swiper-button-next__auto-width-slider',
+          prevEl: '.swiper-button-prev.swiper-button-prev__auto-width-slider',
+        },
+        pagination: {
+          el: '.swiper-pagination-small.swiper-pagination__auto-width-slider'
+        },
+        breakpoints: {
+          768: {
+            spaceBetween: 0
+          }
+        }
+      });
+
+      new Swiper('[auto-width-slider2]', {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        navigation: {
+          nextEl: '.swiper-button-next.swiper-button-next__auto-width-slider2',
+          prevEl: '.swiper-button-prev.swiper-button-prev__auto-width-slider2',
+        },
+        pagination: {
+          el: '.swiper-pagination-small.swiper-pagination__auto-width-slider2'
+        },
+      });
+
+      new Swiper('[auto-width-slider3]', {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        pagination: {
+          el: '.swiper-pagination-small.swiper-pagination__auto-width-slider3'
+        },
+        breakpoints: {
+          768: {
+            spaceBetween: 0
+          }
+        }
+      });
+
     },
     destroy: function () {
       // ... code ...
