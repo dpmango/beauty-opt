eb0d546c3f35c437120f0fe980227486c2e3ac83
mars0092@mail.ru <vecjhj_x.rf>
2019-05-04 01:01:37 +0700

    edite

--- src/js/modules/ui/selectric.js
+++ src/js/modules/ui/selectric.js
@@ -1,23 +1,24 @@
 ////////////////////
 // SELECTRIC PLUGIN
 ////////////////////
-(function($, APP) {
+(function ($, APP) {
   APP.Plugins.Selectric = {
-    init: function() {
+    init: function () {
       var $select = $('[js-select]');
       if ($select.length === 0) return;
 
       $select.selectric({
+        disableOnMobile: false,
+        nativeOnMobile: false,
         maxHeight: 300,
-        arrowButtonMarkup:
-          '<b class="button"><svg class="ico ico-mono-arrowBottom"><use xlink:href="img/sprite-mono.svg#ico-mono-arrowBottom"></use></svg></b>',
-        onInit: function(element, data) {
+        arrowButtonMarkup: '<b class="button"><svg class="ico ico-mono-arrowBottom"><use xlink:href="img/sprite-mono.svg#ico-mono-arrowBottom"></use></svg></b>',
+        onInit: function (element, data) {
           var $elm = $(element),
             $wrapper = $elm.closest('.' + data.classes.wrapper);
 
           $wrapper.find('.label').html($elm.attr('placeholder'));
         },
-        onBeforeOpen: function(element, data) {
+        onBeforeOpen: function (element, data) {
           var $elm = $(element),
             $wrapper = $elm.closest('.' + data.classes.wrapper);
 
@@ -26,7 +27,7 @@
             .data('value', $wrapper.find('.label').html())
             .html($elm.attr('placeholder'));
         },
-        onBeforeClose: function(element, data) {
+        onBeforeClose: function (element, data) {
           var $elm = $(element),
             $wrapper = $elm.closest('.' + data.classes.wrapper);
 
--- src/js/modules/ui/ui_slider.js
+++ src/js/modules/ui/ui_slider.js
@@ -1,27 +1,86 @@
 //////////
 // JQ-UI
 //////////
-(function($, APP) {
+(function ($, APP) {
   APP.Plugins.ui_slider = {
-    init: function() {
+    init: function () {
+
+      var ui_slider_min = 0;
+      var ui_slider_max = 3000;
+
       $('[ui-slider-price]').slider({
         range: true,
-        min: 0,
-        max: 2000,
-        values: [ 128, 1477 ],
-        slide: function( event, ui ) {
+        min: ui_slider_min,
+        max: ui_slider_max,
+        values: [128, 1477],
+        slide: function (event, ui) {
           $('.slider-price__price-min')
-            .html(ui.values[ 0 ] + ' руб.');
+            .html(ui.values[0] + ' руб.');
           $('.slider-price__price-max')
-            .html(ui.values[ 1 ] + ' руб.');
+            .html(ui.values[1] + ' руб.');
+          $('.slider-price__input-min')
+            .val(ui.values[0]);
+          $('.slider-price__input-max')
+            .val(ui.values[1]);
         }
       });
       $('.slider-price__price-min')
         .html($('[ui-slider-price]')
-        .slider( "values", 0 ) + ' руб.');
+          .slider("values", 0) + ' руб.');
       $('.slider-price__price-max')
         .html($('[ui-slider-price]')
-        .slider( "values", 1 ) + ' руб.');
+          .slider("values", 1) + ' руб.');
+      $('.slider-price__input-min')
+        .val($('[ui-slider-price]')
+          .slider("values", 0));
+      $('.slider-price__input-max')
+        .val($('[ui-slider-price]')
+          .slider("values", 1));
+
+      jQuery('.slider-price__input-min').change(function () {
+        var value1 = jQuery('.slider-price__input-min').val();
+        var value2 = jQuery('.slider-price__input-max').val();
+        if (parseInt(value1) > parseInt(value2)) {
+          value1 = value2;
+          jQuery('.slider-price__input-min').val(value1);
+        }
+        jQuery("[ui-slider-price]").slider("values", 0, value1);
+        $('.slider-price__price-min')
+          .html($('[ui-slider-price]')
+            .slider("values", 0) + ' руб.');
+      });
+
+      jQuery('.slider-price__input-max').change(function () {
+        var value1 = jQuery('.slider-price__input-min').val();
+        var value2 = jQuery('.slider-price__input-max').val();
+        if (value2 > ui_slider_max) {
+          value2 = ui_slider_max;
+          jQuery('.slider-price__input-max').val(ui_slider_max)
+        }
+        if (parseInt(value1) > parseInt(value2)) {
+          value2 = value1;
+          jQuery('.slider-price__input-max').val(value2);
+        }
+        jQuery('[ui-slider-price]').slider("values", 1, value2);
+        $('.slider-price__price-max')
+          .html($('[ui-slider-price]')
+            .slider("values", 1) + ' руб.');
+      });
+
+      jQuery('.slider-price input').keypress(function (event) {
+        var key, keyChar;
+        if (!event) var event = window.event;
+
+        if (event.keyCode) key = event.keyCode;
+        else if (event.which) key = event.which;
+
+        if (key == null || key == 0 || key == 8 || key == 13 || key == 9 || key == 46 || key == 37 || key == 39) return true;
+        keyChar = String.fromCharCode(key);
+
+        if (!/\d/.test(keyChar)) return false;
+
+      });
+
     },
   };
 })(jQuery, window.APP);
